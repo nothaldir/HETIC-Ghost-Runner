7e08d79e270849b949c98e391fdb6d8ace20da72
Alex Berneau <haldir.alex@gmail.com>
2018-06-10 03:44:42 +0200

    :sparkles: add firebase to mobile

--- mobile/src/main/java/com/hetic/ghostrunner/MainActivity.java
+++ mobile/src/main/java/com/hetic/ghostrunner/MainActivity.java
@@ -1,13 +1,65 @@
 package com.hetic.ghostrunner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            // already signed in
+            Log.d("AUTH", "USER ALREADY SIGNED IN");
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            // not signed in
+            startActivityForResult(AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setTheme(R.style.AppTheme)
+                .setIsSmartLockEnabled(false)
+                // .setLogo(R.mipmap.ic_launcher_foreground)
+                .setAvailableProviders(
+                    Arrays.asList(
+                        new AuthUI.IdpConfig.EmailBuilder().build(),
+                        new AuthUI.IdpConfig.GoogleBuilder().build()
+                    )
+                )
+                .build(),
+                RC_SIGN_IN
+            );
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                // user logged in
+                Log.d("AUTH", auth.getCurrentUser().getEmail());
+                Intent intent = new Intent(this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+            else {
+                // user not auth
+                Log.d("AUTH", "NOT AUTHENTICATED");
+            }
+        }
     }
 }
