7541293292e4e26e14f9f9acc8d6e0d4ed1fae3c
Alex Berneau <haldir.alex@gmail.com>
2018-06-13 18:21:04 +0200

    :construction: test vibration

--- wear/src/main/java/com/hetic/ghostrunner/MainActivity.java
+++ wear/src/main/java/com/hetic/ghostrunner/MainActivity.java
@@ -1,19 +1,39 @@
 package com.hetic.ghostrunner;
 
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.support.annotation.Nullable;
 import android.support.wearable.activity.WearableActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends WearableActivity {
+public class MainActivity extends WearableActivity implements View.OnClickListener {
 
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        findViewById(R.id.button).setOnClickListener(this);
 
         // Enables Always-on
         setAmbientEnabled();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.button) {
+            Log.d("VIBRATE", "clicked");
+
+            Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+            long[] vibrationPattern = {0, 500, 50, 300};
+            //-1 - don't repeat
+            final int indexInPatternToRepeat = -1;
+            vibrator.vibrate(vibrationPattern, indexInPatternToRepeat);
+        }
     }
 }
